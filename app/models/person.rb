95b2538ce8b5f747b267c4be7fcc39dda8bb41f3
Thiago Oliveira <thiago@signaes.co>
2017-08-18 11:33:46 -0300

    feat: downcases emails before saving to the database

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,8 +1,16 @@
 class Person < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
+  before_save :downcase_email
+
   validates :name, length: { maximum: 60 }
   validates :email, presence: true, length: { maximum: 100 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
 end

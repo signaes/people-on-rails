afa6398530ac800fecd66a4061ff033898209c29
Thiago Oliveira <thiago@signaes.co>
2017-08-18 13:15:37 -0300

    feat: adds Home controller and resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'people/new'
+  root 'home#index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/signup', to: 'people#new'
+
+  resources :people
 end
